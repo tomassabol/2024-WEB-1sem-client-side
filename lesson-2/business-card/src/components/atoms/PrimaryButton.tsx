3a2057d0754a22d65bc47c8745fb8a5453ea02c5
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-05 14:04:21 +0100

    State example, with count in Primary button

--- lesson-2/business-card/src/components/atoms/PrimaryButton.tsx
+++ lesson-2/business-card/src/components/atoms/PrimaryButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./PrimaryButton.module.css";
 
 interface PrimaryButtonProps {
@@ -9,9 +10,25 @@ export default function PrimaryButton({
   buttonText,
   onClick,
 }: PrimaryButtonProps) {
+  const [count, setCount] = useState(0);
+
+  function handleIncreaseCount() {
+    console.log("Hello from handleIncreaseCount");
+
+    //Staticly updating a state value
+    //setCount(3);
+
+    // This will work
+    // setCount(count + 1);
+
+    // This is the best practice,
+    // when working with values that rely on the previous/current value
+    setCount((previousValue) => previousValue + 1);
+  }
+
   return (
-    <button className={styles.main} onClick={onClick}>
-      {buttonText}
+    <button className={styles.main} onClick={handleIncreaseCount}>
+      {buttonText} {count}
     </button>
   );
 }
