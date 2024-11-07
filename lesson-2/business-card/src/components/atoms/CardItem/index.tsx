5b42df6c631228649d676801fe11b1e8c74986e4
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-07 20:14:03 +0100

    reverted changes to index.tsx

--- lesson-2/business-card/src/components/atoms/CardItem/index.tsx
+++ lesson-2/business-card/src/components/atoms/CardItem/index.tsx
@@ -1,12 +1,3 @@
 /* eslint-disable react-refresh/only-export-components */
-//export * from "./CardItem";
-//export { default } from "./CardItem";
-import styles from "./CardItem.module.css";
-
-interface Props {
-  name: string;
-}
-
-export default function CardItem({ name }: Props) {
-  return <div className={styles.card}> {name} </div>;
-}
+export * from "./CardItem";
+export { default } from "./CardItem";
